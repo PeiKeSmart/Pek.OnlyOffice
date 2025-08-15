8a857e84ff25216307a93f7cf26f48cb5070d02e
猿人易 <qcjxberin@outlook.com>
2025-08-15 20:31:22 +0800

    添加 OnlyOffice 配置和启动逻辑
    在 `Pek.OnlyOffice.csproj` 中添加 `Pek.AspNetCore` 和 `Pek.Common` 包引用。
    在 `DHStartup.cs` 中实现 `IPekStartup` 接口，定义服务、中间件、虚拟文件系统和路由配置方法，并实现高德地图服务的请求代理。
    在 `OnlyOfficeSetting.cs` 中定义 `OnlyOfficeSetting` 类，添加 OnlyOffice 地址配置属性及相关描述。

---
+++ Pek.OnlyOffice/OnlyOfficeSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace Pek.OnlyOffice;
+
+/// <summary>OnlyOffice设置</summary>
+[DisplayName("OnlyOffice设置")]
+[Config("AMapSetting")]
+public class OnlyOfficeSetting : Config<OnlyOfficeSetting>
+{
+    /// <summary>
+    /// OnlyOffice地址
+    /// </summary>
+    [Description("OnlyOffice地址")]
+    public String? OnlyOfficeUrl { get; set; }
+}
